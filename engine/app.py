8da6c92adbe32bf4980310470ba6427b61ba95bf
amorgrace <akanforte@gmail.com>
2026-01-10 17:31:06 +0100

    pxl

--- engine/app.py
+++ engine/app.py
@@ -1,8 +1,14 @@
 # app/app.py
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "engine.settings")
+django.setup()
+
 from ninja import NinjaAPI
 from corper.views import router as corper_router
 from vendor.views import router as vendor_router
 from authenticator.views import router as auth_router
+
 
 app = NinjaAPI()
 
