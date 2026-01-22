047a1f0ac6af10b54512df41b04293f31402ff35
amorgrace <akanforte@gmail.com>
2026-01-22 09:54:35 +0100

    ruff fixed linting

--- engine/settings.py
+++ engine/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 from pathlib import Path
 
 import dj_database_url
-from decouple import config
+from decouple import Csv, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 SECRET_KEY_FALLBACKS = []
 DEBUG = config("DEBUG", default=True, cast=bool)
 # ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 AUTH_USER_MODEL = "authenticator.User"
 
